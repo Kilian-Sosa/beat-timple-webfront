f63d9b2da6816901aae24accd217ad5d9f9800c9
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-05-16 20:37:40 +0100

    wip

---
+++ src/components/Home/Introduction/Introduction.tsx
@@ -0,0 +1,14 @@
+import './Introduction.scss'
+
+export const Introduction = () => {
+  return (
+    <>
+    <div className="introduction-cont">
+        <div className="introduction-content">
+            <h1>DISCOVER A NEW MUSIC GAME</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro reiciendis doloribus dicta molestiae aspernatur iusto, incidunt veniam odit beatae? Ex repellat quae ipsam sed eius nesciunt quo repellendus nostrum eaque?</p>
+        </div>
+    </div>
+    </>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import { Header } from "../components/Home/Header/Header";
+import { Introduction } from "../components/Home/Introduction/Introduction";
 
 
 export default function Home() {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      <Introduction />
+    </>
   );
 }
