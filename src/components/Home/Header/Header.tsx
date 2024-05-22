cfc4f160914c95babed8fdbee0b4104ec0dd01b3
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-05-22 20:33:42 +0100

    wip

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,8 +1,22 @@
 import './Header.scss';
 
 export const Header = () => {
+  const logo: string = './assets/images/icons/logoNeonPink.svg'
   return (
     <div className='header-cont'>
+
+      <div className="header-content">
+
+        <img src={logo} alt="" />
+        <h1>IMMERSE IN VR RHYTHM</h1>
+        <h3> Create, Smash, Repeat!</h3>
+
+        <div className="buttons-cont">
+          <button className="btn">
+            <span>download PC demo</span>
+          </button>
+        </div>
+      </div>
 
     </div>
   )
