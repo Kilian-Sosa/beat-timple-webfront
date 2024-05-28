990376ea455c23b04a331eb8441cb7182ca5d111
Kilian Sosa Guillén <kilianjonayyairam@gmail.com>
2024-05-28 12:24:09 +0200

    feat: Separate form content

--- src/components/BeatCreation/BeatForm.tsx
+++ src/components/BeatCreation/BeatForm.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useForm } from "../../hooks/useForm";
+import "./BeatForm.scss";
 import Mark from "../../interfaces/Mark";
 
 // interface BeatFormProps {
@@ -8,15 +9,13 @@ import Mark from "../../interfaces/Mark";
 //   marks: Mark[];
 // }
 
-export const BeatForm: React.FC<any> = ({
-  mark,
-  setMarks,
-  marks
-}) => {
+export const BeatForm: React.FC<any> = ({ mark, setMarks, marks }) => {
   const [formValues, handleInputChange, reset] = useForm(mark);
 
   useEffect(() => {
-    const existingIndex = marks.findIndex((item:any) => item.id === formValues.id);
+    const existingIndex = marks.findIndex(
+      (item: any) => item.id === formValues.id
+    );
     if (existingIndex === -1) {
       setMarks([...marks, formValues]);
       return;
@@ -24,115 +23,120 @@ export const BeatForm: React.FC<any> = ({
     const updatedMarks = [...marks];
     updatedMarks[existingIndex] = formValues;
     setMarks(updatedMarks);
-    console.log(marks);
+    // console.log(marks);
   }, [formValues]);
 
   return (
-    <>
-      {/* <h1>BeatForm</h1> */}
-
-<section className="form-cont">
-      <div className="form-content">
-      <input
-        type="text"
-        name="time"
-        value={formValues.time}
-        onChange={handleInputChange}
-      />
-
-      <span>Side of the Platform</span>
-      <label htmlFor="LeftLocX">Left</label>
-      <input
-        type="radio"
-        id="LeftLocX"
-        name="locationX"
-        value="Left"
-        checked={formValues.locationX === "Left"}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="CenterLocX">Middle</label>
-      <input
-        type="radio"
-        id="CenterLocX"
-        name="locationX"
-        value="Center"
-        checked={formValues.locationX === "Middle"}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="RightLocX">Right</label>
-      <input
-        type="radio"
-        id="RightLocX"
-        name="locationX"
-        value="Right"
-        checked={formValues.locationX === "Right"}
-        onChange={handleInputChange}
-      />
-   </div>   
-<div className="form-content">
-      <span>Vertical Position</span>
-      <label htmlFor="TopLocY">Top</label>
-      <input
-        type="radio"
-        id="TopLocY"
-        name="locationY"
-        value="Top"
-        checked={formValues.locationY === "Top"}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="DownLocY">Bottom</label>
-      <input
-        type="radio"
-        id="DownLocY"
-        name="locationY"
-        value="Bottom"
-        checked={formValues.locationY === "Bottom"}
-        onChange={handleInputChange}
-      />
-   
+    <section className="form-cont">
+      <div className="form-row">
+        <label htmlFor="time">Beat Time</label>
+        <input
+          type="text"
+          name="time"
+          value={formValues.time}
+          onChange={handleInputChange}
+        />
       </div>
-<div className="form-content">
-
-      <span>Hit Side</span>
-      <label htmlFor="LeftHit">Left</label>
-      <input
-        type="radio"
-        id="LeftHit"
-        name="hit"
-        value="Left"
-        checked={formValues.hit === "Left"}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="RightHit">Right</label>
-      <input
-        type="radio"
-        id="RightHit"
-        name="hit"
-        value="Right"
-        checked={formValues.hit === "Right"}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="TopHit">Top</label>
-      <input
-        type="radio"
-        id="TopHit"
-        name="hit"
-        value="Top"
-        checked={formValues.hit === "Top"}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="BottomHit">Bottom</label>
-      <input
-        type="radio"
-        id="BottomHit"
-        name="hit"
-        value="Bottom"
-        checked={formValues.hit === "Bottom"}
-        onChange={handleInputChange}
-      />
-  
-  </div>
-  </section>
-    </>
+      <hr />
+      <div className="form-row">
+        <span>Side of the Platform</span>
+        <div className="form-options">
+          <label htmlFor="LeftLocX">Left</label>
+          <input
+            type="radio"
+            id="LeftLocX"
+            name="locationX"
+            value="Left"
+            checked={formValues.locationX === "Left"}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="CenterLocX">Middle</label>
+          <input
+            type="radio"
+            id="CenterLocX"
+            name="locationX"
+            value="Center"
+            checked={formValues.locationX === "Middle"}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="RightLocX">Right</label>
+          <input
+            type="radio"
+            id="RightLocX"
+            name="locationX"
+            value="Right"
+            checked={formValues.locationX === "Right"}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <hr />
+      <div className="form-row">
+        <span>Vertical Position</span>
+        <div className="form-options">
+          <label htmlFor="TopLocY">Top</label>
+          <input
+            type="radio"
+            id="TopLocY"
+            name="locationY"
+            value="Top"
+            checked={formValues.locationY === "Top"}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="DownLocY">Bottom</label>
+          <input
+            type="radio"
+            id="DownLocY"
+            name="locationY"
+            value="Bottom"
+            checked={formValues.locationY === "Bottom"}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <hr />
+      <div className="form-row">
+        <span>Hit Side</span>
+        <div className="form-options">
+          <label htmlFor="LeftHit">Left</label>
+          <input
+            type="radio"
+            id="LeftHit"
+            name="hit"
+            value="Left"
+            checked={formValues.hit === "Left"}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="RightHit">Right</label>
+          <input
+            type="radio"
+            id="RightHit"
+            name="hit"
+            value="Right"
+            checked={formValues.hit === "Right"}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="TopHit">Top</label>
+          <input
+            type="radio"
+            id="TopHit"
+            name="hit"
+            value="Top"
+            checked={formValues.hit === "Top"}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="BottomHit">Bottom</label>
+          <input
+            type="radio"
+            id="BottomHit"
+            name="hit"
+            value="Bottom"
+            checked={formValues.hit === "Bottom"}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <hr />
+    </section>
   );
 };
