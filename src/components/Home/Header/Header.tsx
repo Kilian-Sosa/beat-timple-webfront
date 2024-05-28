6f280c6ca2199c14b49a150f2f112680a8c9c652
abigailojeda <abigail.arte@gmail.com>
2024-05-28 16:24:39 +0100

    video

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,13 +1,41 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import './Header.scss';
+import YouTube, { YouTubeProps } from 'react-youtube';
+const logo: string = './assets/images/icons/logoNeonPink.svg'
 
 export const Header = () => {
-  const logo: string = './assets/images/icons/logoNeonPink.svg'
+
+  const [showVideo, setShowVideo] = useState(true)
+
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className='header-cont'>
 
-      <div className="header-content">
+{
+  showVideo && (
+    <div className='video-cont'>
+      <button onClick={() => setShowVideo(false)}>&times;</button>
+      <YouTube videoId="cqg_ytPT428" opts={opts} onReady={onPlayerReady} />
+    </div>
+  )
 
+}
+
+      <div className="header-content">
         <img src={logo} alt="" />
         <h1>IMMERSE IN VR RHYTHM</h1>
         <h3> Create, Smash, Repeat!</h3>
@@ -20,7 +48,7 @@ export const Header = () => {
           </button>
           </a>
 
-          <button className='btn purple'>
+          <button onClick={() => setShowVideo(true)} className='btn purple'>
               <span>TRAILER</span>
             </button>
 
